92fa864d963a1f57477cc76d1102021a9cc98cf7
Geir Sagberg <geir.sagberg@gmail.com>
2025-09-16 10:10:45 +0200

    Remove Oracle DB

--- scripts/02_init.sql
+++
@@ -1,7 +0,0 @@
--- Database initialization script for TN-ITS prototype
--- Connect to the FREEPDB1 pluggable database and grant permissions
-
-ALTER SESSION SET CONTAINER=FREEPDB1;
-
--- Grant DBA privileges to the tnits user for development convenience
-GRANT DBA TO tnits;
