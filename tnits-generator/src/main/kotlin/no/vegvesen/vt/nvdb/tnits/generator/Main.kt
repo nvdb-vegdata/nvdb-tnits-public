26d92051a4083f1875f26035543313c782756eaf
Geir Sagberg <geir.sagberg@gmail.com>
2025-09-24 14:56:22 +0200

    Move generator to submodule

--- tnits-generator/src/main/kotlin/no/vegvesen/vt/nvdb/tnits/generator/Main.kt
+++
@@ -1,8 +0,0 @@
-package no.vegvesen.vt.nvdb.tnits.generator
-
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
-
-fun main() {
-    println("Hello World! The time is: ${LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)}")
-}
