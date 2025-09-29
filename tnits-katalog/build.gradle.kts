b356092a30a5fac47e1b425c463a3a94cb93c03d
Erik Aadal Gustafsson <erik.gustafsson@vegvesen.no>
2025-09-29 13:27:03 +0200

    DD-1543: Publish Spring Boot JAR for tnita-katalog.

--- tnits-katalog/build.gradle.kts
+++ tnits-katalog/build.gradle.kts
@@ -1,6 +1,16 @@
+import gradle.kotlin.dsl.accessors._734a7a566e0c761836055f103d9b4672.publishing
+
 plugins {
     id("tnits-conventions")
     id("org.springframework.boot") version "3.5.6"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifact(tasks["bootJar"])
+        }
+    }
 }
 
 dependencies {
