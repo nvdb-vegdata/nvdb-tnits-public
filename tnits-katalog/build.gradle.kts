c49f9276a9bac688e11d0b8ab519737ad6422306
Erik Aadal Gustafsson <erik.gustafsson@vegvesen.no>
2025-09-30 09:38:37 +0200

    DD-1543: Add bootJar to publication.

--- tnits-katalog/build.gradle.kts
+++ tnits-katalog/build.gradle.kts
@@ -3,6 +3,14 @@ plugins {
     id("org.springframework.boot") version "3.5.6"
 }
 
+publishing {
+    publications {
+        publications.getByName<MavenPublication>("maven") {
+            artifact(tasks.bootJar)
+        }
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.5.6")
 }
