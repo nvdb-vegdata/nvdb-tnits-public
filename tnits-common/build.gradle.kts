21dd6dc6b2a450732688183317facb1b048a6742
Geir Sagberg <geir.sagberg@gmail.com>
2025-11-29 15:47:54 +0100

    Update testcontainers
    Fixes bug with latest Docker Desktop

--- tnits-common/build.gradle.kts
+++ tnits-common/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.9.0")
 
-    testFixturesApi(platform("org.testcontainers:testcontainers-bom:2.0.1"))
+    testFixturesApi(platform("org.testcontainers:testcontainers-bom:2.0.2"))
 
     // Testcontainers for integration testing
     testFixturesApi("org.testcontainers:testcontainers-minio")
